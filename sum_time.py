ae2821dd13572c22ecb1ff1c46933082531c8a96
Larry Jones <mrwizard82d1@gmail.com>
2010-10-22 13:58:15 -0500

    Created new utility to summarize time.
    This script is contained in one file and does not need multiple pipes.
    
    Fixed issues with splitting strings (split(sep, max) returns a tuple of
    max + 1 items).
    Fixed issue with handling empty lines.

---
+++ sum_time.py
@@ -0,0 +1,68 @@
+#! python
+
+"""Summarizes time into tasks."""
+
+
+from datetime import datetime
+import sys
+
+
+class Task(object):
+    """Defines a task."""
+
+    def __init__(self, start_line):
+        """Constructs an instance from a text line describing its start."""
+        day_text, time_point_text, name = start_line.split(None, 2)
+        self.name = name.strip()
+        year, month, day = [int(part) for part in ((day_text[0:4]),
+                                                   (day_text[4:6]),
+                                                   (day_text[6:]))]
+        hour, minute = [int(part) for part in (time_point_text[0:2],
+                                               time_point_text[2:])]
+        self.start_time = datetime(year, month, day, hour, minute, 0)
+        self.end_time = self.start_time
+
+    def duration(self):
+        """Determines the duration of this task."""
+        return (self.end_time - self.start_time)
+        
+        
+def make_tasks(infile):
+    """Summarizes time into tasks."""
+
+    # Create all the tasks
+    task_lines = [l for l in infile.readlines() if len(l.strip()) > 0]
+    result = [Task(l) for l in task_lines]
+    
+
+    # End of each task is the start of the next task
+    end_times = [t.start_time for t in result[1:]]
+    for task_ndx in range(len(end_times)):
+        result[task_ndx].end_time = end_times[task_ndx]
+        
+    return result
+
+
+def print_summary(summary):
+    """Print a task duration summary."""
+    names = summary.keys()
+    names.sort()
+    for name in names:
+        hours = summary[name] // 3600
+        minutes = (summary[name] % 3600) // 60
+        duration = hours + (minutes / 60.0)
+        print('{0:16}{1:.2f}'.format(name, duration))
+
+
+def sum_time(tasks):
+    """Summarizes the time for all similar tasks."""
+    
+    result = {}
+    for t in tasks:
+        result[t.name] = (result.get(t.name, 0) + t.duration().seconds)
+    return result
+
+
+if __name__ == '__main__':
+    infile = sys.stdin
+    print_summary(sum_time(make_tasks(infile)))
